efbf8f99bcd9deb82efa22283db2a2a436177abd
israrulhaq-IFL <israr.haq@igc.com.pk>
2024-10-04 22:47:55 +0500

    task adding pop will show now

--- task-manager-frontend/src/App.js
+++ task-manager-frontend/src/App.js
@@ -5,11 +5,14 @@ import TaskList from './components/TaskList';
 import TaskForm from './components/TaskForm';
 import Header from './components/Header'; // Import Header
 import Footer from './components/Footer'; // Import Footer
-import { Container, Row, Col, Button } from 'react-bootstrap';
+
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap';
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 console.log('API_BASE_URL:', API_BASE_URL); // Debugging
+
+
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -55,22 +58,27 @@ function App() {
       <Header /> {/* Add Header */}
       <Container>
         <Row className="justify-content-center mt-5">
-          <Col xs={12} md={8} lg={6}>
-            <div className="text-center">
+          <Col xs={12} md={10} lg={8}>
+            <div className="text-center mb-4">
               <h1>Task Management Application</h1>
             </div>
-            <div className="text-center mt-3">
-              {showForm ? (
-                <TaskForm addTask={addTask} />
-              ) : (
-                <Button variant="primary" onClick={() => setShowForm(true)}>Add Task</Button>
-              )}
+            <div className="text-center mb-4">
+              <Button variant="primary" onClick={() => setShowForm(true)}>Add Task</Button>
             </div>
             <TaskList tasks={tasks} onDelete={deleteTask} />
           </Col>
         </Row>
       </Container>
       <Footer /> {/* Add Footer */}
+
+      <Modal show={showForm} onHide={() => setShowForm(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TaskForm addTask={addTask} />
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
